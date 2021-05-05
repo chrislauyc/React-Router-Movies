ae6bc8052b83e16ea37e1d6b57aab5293c9483f3
Chris Lau <chrislyc1991@gmail.com>
2021-05-04 22:15:38 -0600

    completed MVP

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-
+import {BrowserRouter} from 'react-router-dom';
 // You'll need to wrap <App /> for routing to work
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+    {/* A <Router> that uses the HTML5 history API (pushState, replaceState and the popstate event) to keep your UI in sync with the URL. */}
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root')
+);
